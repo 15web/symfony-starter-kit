09da9c65c82a17b6985410f1ec24aa3acacc9d81
Balobanov Dmitriy <balobanov.dmitriy.78@gmail.com>
2022-05-27 16:37:01 +0300

    feat(ci): Добавил проверки твига и контейнера зависимостей

--- bin/check-code-quality.bash
+++ bin/check-code-quality.bash
@@ -5,3 +5,5 @@ set -Eeuo pipefail
 ./bin/docker-run-cmd.bash vendor/bin/php-cs-fixer fix --dry-run --diff --config php-cs-fixer-config.php
 ./bin/docker-run-cmd.bash vendor/bin/phpstan analyse -c phpstan-config.neon
 ./bin/docker-run-cmd.bash vendor/bin/phpcs --colors -p
+./bin/docker-run-cmd.bash bin/console lint:twig
+./bin/docker-run-cmd.bash bin/console lint:container
