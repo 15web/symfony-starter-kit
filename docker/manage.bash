a23e7138b4decdef14911a11464fcf335cc4739e
Anton Derevyagin <al.derevyagin@gmail.com>
2022-06-03 19:07:56 +0700

    feat(tests): Подключил докер
    META: #86021

---
+++ docker/manage.bash
@@ -0,0 +1,142 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+os-depend-sed-in-place() {
+    if [ "$(uname -s)" == 'Darwin' ]; then
+        sed -i '' "$@";
+    else
+        sed --in-place "$@";
+    fi
+}
+
+setupEnvs() {
+    [ ! -f ./.env ] && cp ./.env.dist ./.env
+
+    COMPOSE_FILE_ENV="COMPOSE_FILE='docker-compose.local.yml'";
+    if [ "$(grep 'COMPOSE_FILE' ./.env)" != '' ]; then
+        os-depend-sed-in-place -E "s|^.*COMPOSE_FILE=.*$|$COMPOSE_FILE_ENV|" ./.env;
+    else
+        echo "$COMPOSE_FILE_ENV" >> ./.env;
+    fi
+
+    USER_ID_ENV="USER_ID=$(id -u)";
+    if [ "$(grep 'USER_ID' ./.env)" != '' ]; then
+        os-depend-sed-in-place -E "s|^.*USER_ID=.*$|$USER_ID_ENV|" ./.env;
+    else
+        echo "$USER_ID_ENV" >> ./.env;
+    fi
+
+    [ ! -f ./backend/.env ] && cp ./backend/.env.dist ./backend/.env
+    [ ! -f ./mysql/.env ] && cp ./mysql/.env.dist ./mysql/.env
+
+    echo 'Envs set up!';
+}
+
+install() {
+    docker-compose build
+
+    runBackend composer install --no-scripts --prefer-dist
+
+    docker-compose up --detach --force-recreate --remove-orphans
+
+    runBackend ./bin/console doctrine:migrations:migrate --no-interaction
+
+    echo "Done!"
+}
+
+up() {
+    docker-compose up -d --force-recreate --remove-orphans
+}
+
+down() {
+    docker-compose down --remove-orphans
+}
+
+update() {
+    docker-compose pull
+    docker-compose build --pull
+    runBackend composer update
+}
+
+build() {
+    docker-compose build
+    runBackend composer install --no-scripts --prefer-dist
+    docker-compose up -d --force-recreate --remove-orphans
+}
+
+runBackend() {
+    docker-compose run --rm backend "$@"
+}
+
+logs() {
+    docker-compose logs "$@"
+}
+
+COMMAND=$1
+case $COMMAND in
+    install | i)
+        setupEnvs
+        install
+        ;;
+    up | u)
+        setupEnvs;
+        up;
+        ;;
+    down | d)
+        setupEnvs;
+        down;
+        ;;
+    update | upd)
+        setupEnvs;
+        update;
+        ;;
+    build | b)
+        setupEnvs;
+        build;
+        ;;
+    run-backend | rb)
+        setupEnvs;
+
+        ARGS_WITHOUT_FIRST="${@:2}"
+        runBackend $ARGS_WITHOUT_FIRST;
+        ;;
+    check | c)
+        setupEnvs;
+
+        runBackend composer check
+        ;;
+    fix | f)
+        setupEnvs;
+
+        runBackend composer fix
+        ;;
+    logs | l)
+        setupEnvs;
+
+        ARGS_WITHOUT_FIRST="${@:2}"
+        logs $ARGS_WITHOUT_FIRST;
+        ;;
+    hooks-install | hi)
+        printf '#!/usr/bin/env sh\n\n./docker/manage.bash check;\n' > .git/hooks/pre-commit;
+
+        chmod +x .git/hooks/pre-commit;
+        ;;
+    setup-envs | se)
+        setupEnvs;
+        ;;
+    *)
+        echo 'Unknown command. Available:
+            install[i],
+            up[u],
+            down[d],
+            update[upd],
+            build[b],
+            run-backend[rb],
+            logs[l],
+            check[c],
+            fix[f],
+            hooks-install[hi],
+            setup-envs[se].'
+        ;;
+esac
