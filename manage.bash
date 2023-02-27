5a0d45132cc105483c3e7753bc06e0242db85410
Anton Derevyagin <15webderevyagin@gmail.com>
2023-02-28 02:26:00 +0700

    feat(docker): Исправил ошибку с .env файлом; обновил пути к manage.bash в README.md
    META: #89011

--- manage.bash
+++ manage.bash
@@ -14,6 +14,11 @@ COMPOSE_ENV_PATH="$(realpath ./.env)"
 COMPOSE_DIST_ENV_PATH="$(realpath ./docker/.env.dist)"
 
 setupEnvs() {
+    # У docker compose странный баг: когда ./.env загружен, compose пытается загрузить ./docker/.env,
+    # вероятно из-за того, что там находится docker-compose.local.yml,
+    # это приводит к непредсказуемому поведению и ошибкам
+    rm -f ./docker/.env
+
     [ ! -f "${COMPOSE_ENV_PATH}" ] && cp "${COMPOSE_DIST_ENV_PATH}" "${COMPOSE_ENV_PATH}"
 
     replace_env "${COMPOSE_ENV_PATH}" 'COMPOSE_PROJECT_NAME' 'symfony-starter-kit-local'
