bb04aab5cd6547abe9e90782885d72888bec16bc
Zayan Goripov <15webgoripov@gmail.com>
2023-06-27 12:18:20 +0300

    feat(php-cs-fixer): Заменил аннотации на атрибуты в TestDox фиксере

--- backend/src-dev/PHPCsFixer/PhpUnit/TestdoxForMethods.php
+++ backend/src-dev/PHPCsFixer/PhpUnit/TestdoxForMethods.php
@@ -4,20 +4,16 @@ declare(strict_types=1);
 
 namespace Dev\PHPCsFixer\PhpUnit;
 
-use PhpCsFixer\DocBlock\DocBlock;
-use PhpCsFixer\Tokenizer\Analyzer\WhitespacesAnalyzer;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
 use PhpCsFixer\Tokenizer\TokensAnalyzer;
-use PhpCsFixer\WhitespacesFixerConfig;
 
 /**
- * Добавляет методам теста аннотацию testdox
+ * Добавляет методам теста атрибут testdox
  */
 final readonly class TestdoxForMethods
 {
     public function __construct(
-        private WhitespacesFixerConfig $whitespacesConfig,
         private DocCommentHelper $commentHelper
     ) {
     }
@@ -35,58 +31,10 @@ final readonly class TestdoxForMethods
             $testdoxIndex = $tokens->getPrevNonWhitespace($index);
             $testdoxIndex = $tokens->getPrevNonWhitespace($testdoxIndex);
 
-            if ($this->commentHelper->isPHPDoc($tokens, $testdoxIndex)) {
-                $this->updateDocBlockIfNeeded($tokens, $testdoxIndex);
-            } else {
-                $this->addTestdox($tokens, $testdoxIndex);
+            if (!$this->commentHelper->hasTestDoxAttribute($tokens, $testdoxIndex)) {
+                $this->commentHelper->addTestdoxAttribute($tokens, $testdoxIndex);
             }
         }
-    }
-
-    /**
-     * @param Tokens<Token> $tokens
-     */
-    private function addTestdox(Tokens $tokens, int $docBlockIndex): void
-    {
-        $lineEnd = $this->whitespacesConfig->getLineEnding();
-        $originalIndent = WhitespacesAnalyzer::detectIndent($tokens, $tokens->getNextNonWhitespace($docBlockIndex));
-        $toInsert = [
-            new Token([T_DOC_COMMENT, '/**'.$lineEnd."{$originalIndent} * @testdox TODO: Опиши что делает данный метод".
-                $lineEnd."{$originalIndent} */"]),
-            new Token([T_WHITESPACE, $lineEnd.$originalIndent]),
-        ];
-        $index = $tokens->getNextMeaningfulToken($docBlockIndex);
-        $tokens->insertAt($index, $toInsert);
-    }
-
-    /**
-     * @param Tokens<Token> $tokens
-     */
-    private function updateDocBlockIfNeeded(Tokens $tokens, int $docBlockIndex): void
-    {
-        $doc = new DocBlock($tokens[$docBlockIndex]->getContent());
-        if (!empty($doc->getAnnotationsOfType('testdox'))) {
-            return;
-        }
-        $doc = $this->commentHelper->makeDocBlockMultiLineIfNeeded($doc, $tokens, $docBlockIndex);
-        $lines = $this->updateDocBlock($doc, $tokens, $docBlockIndex);
-        $lines = implode('', $lines);
-
-        $tokens[$docBlockIndex] = new Token([T_DOC_COMMENT, $lines]);
-    }
-
-    /**
-     * @param Tokens<Token> $tokens
-     */
-    private function updateDocBlock(DocBlock $docBlock, Tokens $tokens, int $docBlockIndex): array
-    {
-        $lines = $docBlock->getLines();
-        $originalIndent = WhitespacesAnalyzer::detectIndent($tokens, $docBlockIndex);
-        $lineEnd = $this->whitespacesConfig->getLineEnding();
-        array_splice($lines, -1, 0, $originalIndent.' *'.$lineEnd.$originalIndent.
-            ' * @testdox TODO: Опиши что делает данный метод'.$lineEnd);
-
-        return $lines;
     }
 
     /**
@@ -106,7 +54,7 @@ final readonly class TestdoxForMethods
         }
 
         $docBlockIndex = $this->commentHelper->getDocBlockIndex($tokens, $index);
-        if (!$this->commentHelper->isPHPDoc($tokens, $docBlockIndex)) {
+        if (!$this->commentHelper->isAttribute($tokens, $docBlockIndex)) {
             return false;
         }
 
