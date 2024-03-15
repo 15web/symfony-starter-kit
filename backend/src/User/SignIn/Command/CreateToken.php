9e9ed0e5465198c3df2f317e0fcf117b298bce65
Maxim Podtykanov <supermaximal23@gmail.com>
2024-03-15 23:08:11 +0300

    feat (auth): Подключил хранение токенов в шифрованном виде
    Meta: #98076

--- backend/src/User/SignIn/Command/CreateToken.php
+++ backend/src/User/SignIn/Command/CreateToken.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\User\SignIn\Command;
 
 use App\Infrastructure\AsService;
+use App\Infrastructure\Hasher;
 use App\User\SignIn\Domain\UserToken;
 use App\User\SignIn\Domain\UserTokenRepository;
+use App\User\SignIn\Service\AuthTokenHasher;
 use App\User\User\Domain\UserId;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Uid\Uuid;
 
 /**
@@ -16,11 +19,22 @@ use Symfony\Component\Uid\Uuid;
 #[AsService]
 final readonly class CreateToken
 {
-    public function __construct(private UserTokenRepository $userTokenRepository) {}
+    public function __construct(
+        private UserTokenRepository $userTokenRepository,
+        #[Autowire(service: AuthTokenHasher::class)]
+        private Hasher $hasher
+    ) {}
 
-    public function __invoke(UserId $userId, Uuid $userTokenId): void
+    /**
+     * @param non-empty-string $token
+     */
+    public function __invoke(UserId $userId, Uuid $userTokenId, string $token): void
     {
-        $userToken = new UserToken($userTokenId, $userId);
+        $userToken = new UserToken(
+            id: $userTokenId,
+            userId: $userId,
+            hash: $this->hasher->hash($token)
+        );
         $this->userTokenRepository->add($userToken);
     }
 }
--- backend/src/User/User/Http/TokenManager.php
+++ backend/src/User/User/Http/TokenManager.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\User\User\Http;
 
 use App\Infrastructure\AsService;
+use App\Infrastructure\Hasher;
 use App\User\SignIn\Domain\UserToken;
 use App\User\SignIn\Domain\UserTokenRepository;
+use App\User\SignIn\Service\AuthTokenHasher;
+use App\User\SignIn\Service\AuthTokenService;
 use DomainException;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Uid\Uuid;
 
 /**
  * Хранилище токена пользователя, используется совместно с атрибутом IsGranted.
@@ -21,6 +24,9 @@ final readonly class TokenManager
 
     public function __construct(
         private UserTokenRepository $userTokenRepository,
+        private AuthTokenService $authTokenService,
+        #[Autowire(service: AuthTokenHasher::class)]
+        private Hasher $hasher
     ) {}
 
     public function getToken(Request $request): UserToken
@@ -31,13 +37,23 @@ final readonly class TokenManager
             throw new TokenException('Не передан токен');
         }
 
-        if (!Uuid::isValid($apiToken)) {
-            throw new TokenException('Невалидный токен');
-        }
+        /**
+         * @var non-empty-string $apiToken
+         */
+        $authToken = $this->authTokenService->parseToken($apiToken);
 
         try {
-            $userToken = $this->userTokenRepository->getById(Uuid::fromString($apiToken));
+            $userToken = $this->userTokenRepository->getById($authToken->tokenId);
         } catch (DomainException) {
+            throw new TokenException('Токен не найден');
+        }
+
+        if (
+            !$this->hasher->verify(
+                data: $authToken->token,
+                hash: $userToken->getHash()
+            )
+        ) {
             throw new TokenException('Токен не найден');
         }
 
