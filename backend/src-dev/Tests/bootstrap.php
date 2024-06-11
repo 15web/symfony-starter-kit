a6312996b3747fb7e3898876c79f62f0edd2537d
Maxim Podtykanov <supermaximal23@gmail.com>
2024-06-11 23:36:16 +0300

    fix (tests): Пофиксил проблему с тестами
    Meta: #99261

--- backend/src-dev/Tests/bootstrap.php
+++ backend/src-dev/Tests/bootstrap.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require dirname(__DIR__).'/../vendor/autoload.php';
 
@@ -11,3 +12,5 @@ if (file_exists(dirname(__DIR__).'/../config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/../.env');
 }
+
+set_exception_handler([new ErrorHandler(), 'handleException']);
