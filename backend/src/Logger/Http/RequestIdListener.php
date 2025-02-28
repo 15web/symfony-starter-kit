2badb2934187145cce53f0a721cd514001903fac
Pavel Prokofiev <15webprokofiev@gmail.com>
2025-02-28 13:22:29 +0300

    feat(logger): добавил запись авторизованного пользователя в логи

--- backend/src/Logger/Http/RequestIdListener.php
+++ backend/src/Logger/Http/RequestIdListener.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Infrastructure\Request;
+namespace App\Logger\Http;
 
 use App\Infrastructure\AsService;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
